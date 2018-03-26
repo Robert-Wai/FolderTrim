dc177b41d2df68922dbdd5554ae1b6d6eb728ee2
Robert <rwai@osisoft.com>
2018-03-25 18:45:33 -0700

    added a tray icon

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 const path = require('path');
 const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow = null;
+let tray = null;
+const iconPath = path.join(__dirname, '/images/Folder_grey_16x.png');
 
 function createMainWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: iconPath,
+        darkTheme: true
+    });
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
@@ -28,14 +35,31 @@ function createMainWindow() {
         // when you should delete the corresponding element.
         mainWindow = null;
     });
+
+    // intercept close event to hide window instead
+    mainWindow.on('close', (e) => {
+        if (!app.isQuitting) {
+            e.preventDefault();
+            mainWindow.hide();
+        }
+    });
+
+    // add a tray
+    tray = new Tray(iconPath);
+    const trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
+    tray.setToolTip("FolderTrim");
+    tray.setContextMenu(trayMenu);
+    tray.on('double-click', () => {
+        mainWindow.show();
+    });
 }
 
 const mainMenuTemplate = [
     {
-        label:'File',
-        submenu:[
+        label: 'File',
+        submenu: [
             {
-                label:'Quit',
+                label: 'Quit',
                 accelerator: 'CmdOrCtrl+Q',
                 click() {
                     app.quit();
@@ -44,21 +68,30 @@ const mainMenuTemplate = [
         ]
     },
     {
-        label:'Dev Tools',
-        submenu:[
+        label: 'Dev Tools',
+        submenu: [
             {
-                label:'Toggle Developer Tools',
+                label: 'Toggle Developer Tools',
                 accelerator: 'F12',
                 click(menuItem, activeWindow) {
                     activeWindow.toggleDevTools();
                 }
             },
             {
-                role:'reload'
+                role: 'reload'
             }
         ]
     }
 ];
+
+const trayMenuTemplate = [
+    {
+        label: 'Quit FolderTrim',
+        click() {
+            app.quit();
+        }
+    }
+]
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -81,3 +114,5 @@ app.on('activate', () => {
         createMainWindow();
     }
 });
+
+app.on('before-quit', ()=> { app.isQuitting = true});
